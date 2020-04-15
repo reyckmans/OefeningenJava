c479f443bc5909ebbe216962b8960c0e95c8ef05
reyckmans <robbee14@gmail.com>
2020-04-15 13:40:38 +0200

    gip

--- src/gip/Starter.java
+++ src/gip/Starter.java
@@ -2,6 +2,8 @@ package gip;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -25,6 +27,18 @@ public class Starter {
 		char c = chars.charAt(rnd.nextInt(chars.length()));
 		System.out.println(rnd); //test
 		
+		// random vraag kiezen
+		Random rndV = new Random();
+	    List<String> givenList = Arrays.asList("Een beroep met een….", "Een zoogdier…", "Een jongensnaam…", "Een meisjesnaam");
+	    
+	    int numberOfElements = 2;
+		
+	    for (int i = 0; i < numberOfElements; i++) {
+	        int randomIndex = rndV.nextInt(givenList.size());
+	        String randomElement = givenList.get(randomIndex);
+	        givenList.remove(randomIndex);
+	    }
+		
 		//frame
 		JFrame frame = new JFrame("Pim Pam Pet Online Edition");
 		frame.setSize(800,800);
@@ -41,16 +55,34 @@ public class Starter {
 		JButton buttonL = new JButton("Letter");
 		buttonL.setSize(80,50);
 		buttonL.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 labelL.setText("" + chars.charAt(rnd.nextInt(chars.length())));
+			}
+		});
+		
+		//label voor vragen
+		JLabel labelV = new JLabel();
+		labelV.setSize(80,100);
+		
+		// button van de vraag
+		JButton buttonV = new JButton("Vraag");
+		buttonV.setSize(80,50);
+		buttonL.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				labelL.setText("" + rnd);
+				 labelL.setText(////////////);
 			}
 		});
+		
 		
 		// zichtbaarheid functie
 		panel.add(buttonL);
 		panel.add(labelL);
+		panel.add(buttonV);
+		panel.add(labelV);
 		frame.setVisible(true);
 		
 		
