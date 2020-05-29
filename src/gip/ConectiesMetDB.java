4d04eb5bd9790518fb7bfaa763ac98f3e90fb5b7
nuytsm <marknuyts@gmail.com>
2020-05-29 13:50:04 +0200

    Correction to database URI Move preparedstatement to ConnectiesMetDB add main method in controlevraag: maakt testen makkelijker

--- src/gip/ConectiesMetDB.java
+++ src/gip/ConectiesMetDB.java
@@ -1,6 +1,10 @@
 package gip;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  * 
  * Dit is mijn klasse die er voor zorgt dat mijn SQL datebase geconecteerd staat met mijn spel, zodat ik de vragen en voorspelde antwoorden waarmee men verbeter uit mijn lijst kan halen en mee kan werken.
@@ -12,14 +16,29 @@ import java.sql.DriverManager;
 
 public class ConectiesMetDB {
 	
-	public ConectiesMetDB con = new ConectiesMetDB();
+	public Connection con;
 	
 	public ConectiesMetDB() {
 		try {
-			con = (ConectiesMetDB) DriverManager.getConnection("jdbc:sqlite:databasePimPamPet.db");
+			con = DriverManager.getConnection("jdbc:sqlite:src/gip/databasePimPamPet.db");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Error connecting to database");
 		}
+	}
+	
+	public boolean retrieveFromDB(String antwoordGebruiker) {
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("SELECT * FROM oplossingen WHERE oplossing LIKE ?");
+			ps.setString(1, antwoordGebruiker + "%");
+			ps.execute();
+			ResultSet resultset = ps.getResultSet();
+			return resultset.getFetchSize() > 0;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
--- src/gip/ControleVraag.java
+++ src/gip/ControleVraag.java
@@ -20,16 +20,11 @@ public class ControleVraag {
 
 	int score = 0;
 	
-	ConectiesMetDB con = new ConectiesMetDB();
+	ConectiesMetDB connectiesMetDB = new ConectiesMetDB();
 	
 	public boolean checkAntwoord(String antwoord,String letter) {
 		if (antwoord.toLowerCase().toCharArray()[0]== letter.toLowerCase().toCharArray()[0]) {
-			try {
-				PreparedStatement ps = con.PrepareStatement("SELECT * FROM oplossingen WHERE oplossing LIKE ?");
-				ps.setString(1, antwoord + "%");
-				ps.execute();
-				ResultSet resultset = ps.getResultSet();
-				if (resultset.getFetchSize() > 0) {
+				if (connectiesMetDB.retrieveFromDB(antwoord)) {
 					score++;
 					System.out.println("juist");
 					return true;
@@ -38,11 +33,12 @@ public class ControleVraag {
 					System.out.println("fout");
 					return false;
 				}
-			} catch (SQLException e) {
-				// TODO: handle exception
-				e.printStackTrace();
 			}
-		}
-	return false;
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		ControleVraag controle = new ControleVraag();
+		System.out.println(controle.checkAntwoord("mazda", "m"));
 	}
 }
